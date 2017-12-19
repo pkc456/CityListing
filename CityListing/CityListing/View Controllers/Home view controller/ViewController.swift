eac4295494c10d5b3f583edaf6f299f5139c4595
Pardeep <pkc456@gmail.com>
2017-12-19 23:55:13 +0530

    1) Added tableview to home view controller 2) UI changes at splash screen

--- CityListing/CityListing/View Controllers/Home view controller/ViewController.swift
+++ CityListing/CityListing/View Controllers/Home view controller/ViewController.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    //MARK:- Properties
+    @IBOutlet weak var tableviewHome: UITableView!
+    fileprivate var arrayCity:[City] = []
+
+    //MARK:- View life cycle method
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setUpUIElements()
+        loadDataFromDatabase()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK:- User defined method
+    
+    //Initial UI set up will be done in this method
+    private func setUpUIElements(){
+        tableviewHome.estimatedRowHeight = 50.0
+        tableviewHome.rowHeight = UITableViewAutomaticDimension
+    }
+    
+    //Load city listing from database
+    private func loadDataFromDatabase(){
+        
+        let managedObjectContext = PersistanceService.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "City")
+        do {
+            arrayCity = try managedObjectContext.fetch(fetchRequest) as! [City]
+        } catch {
+            print("Failed to retrieve record")
+            print(error)
+        }
     }
 
-
+    // MARK: - Table view data source
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayCity.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "cellReuseIdentifierN"
+        var cell:UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        
+        if (cell == nil){
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+            cell?.accessoryType = .disclosureIndicator
+            cell?.textLabel?.numberOfLines = 0
+        }
+        
+        let cityData = arrayCity[indexPath.row]
+        cell?.textLabel?.text = cityData.name
+        return cell!
+    }
+    
 }
 
